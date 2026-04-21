6830cdb979e49c7ffd52248722c49acb79e87438
Shivani Shrivastava <shivani.shri0901@gmail.com>
2026-04-21 22:44:34 +0530

    Update nexus_dashboard.py

--- nexus_dashboard.py
+++ nexus_dashboard.py
@@ -9,7 +9,7 @@ import numpy as np
 import plotly.express as px
 import streamlit as st
 
-st.set_page_config(page_title="Nexus Analytics", page_icon="📊", layout="wide")
+st.set_page_config(page_title="Nexus Analytics", page_icon="📊", layout="wide",initial_sidebar_state="expanded")
 
 
 # ============================================================
